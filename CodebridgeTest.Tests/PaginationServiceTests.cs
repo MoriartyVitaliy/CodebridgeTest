09b23e2fccb0cc90be55e1ae094bf8a887d90cd2
MoriartyVitaliy <vgriss@knu.ua>
2025-11-04 01:35:01 +0200

    Add pagination service tests
    Introduce `PaginationServiceTests` class with a test method
    `ApplyPagination_Should_Return_Correct_Page` to validate
    the pagination logic in the `PaginationService`. The test
    ensures that the correct items and total count are returned
    for a given page number and page size.

---
+++ CodebridgeTest.Tests/PaginationServiceTests.cs
@@ -0,0 +1,29 @@
+﻿using CodebridgeTest.Application.Services;
+using CodebridgeTest.Core.Common.Pagination;
+
+
+namespace CodebridgeTest.Tests
+{
+    public class PaginationServiceTests
+    {
+        [Fact]
+        public void ApplyPagination_Should_Return_Correct_Page()
+        {
+            var service = new PaginationService();
+            var data = Enumerable.Range(1, 10).AsQueryable();
+            var request = new PaginationRequest { PageNumber = 2, PageSize = 3 };
+
+            var resultQuery = service.ApplyPagination(data, request);
+            var result = new PagedResult<int>
+            {
+                items = resultQuery.ToList(),
+                totalItems = data.Count(),
+                pageNumber = request.PageNumber,
+                pageSize = request.PageSize
+            };
+
+            Assert.Equal(10, result.totalItems);
+            Assert.Equal(new List<int> { 4, 5, 6 }, result.items);
+        }
+    }
+}
