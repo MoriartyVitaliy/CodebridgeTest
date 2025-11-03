0ed38b5160eb4732ec9215c35a1f56e2dbdeba75
MoriartyVitaliy <vgriss@knu.ua>
2025-11-04 01:34:45 +0200

    Add unit tests for DogSortService
    Introduced a new test class `DogSortServiceTests` in the `CodebridgeTest.Tests` namespace. This class contains two unit tests:
    1. `ApplySorting_Should_Sort_Seeded_Dogs_By_Name_Ascending` to verify sorting by name in ascending order.
    2. `ApplySorting_Should_Sort_Seeded_Dogs_By_Weight_Desc` to check sorting by weight in descending order.
    
    Both tests ensure the correctness of the sorting functionality in the `DogSortService` class.

---
+++ CodebridgeTest.Tests/DogSortServiceTests.cs
@@ -0,0 +1,43 @@
+﻿using CodebridgeTest.Application.Services;
+using CodebridgeTest.Core.Models;
+
+namespace CodebridgeTest.Tests
+{
+    public class DogSortServiceTests
+    {
+        [Fact]
+        public void ApplySorting_Should_Sort_Seeded_Dogs_By_Name_Ascending()
+        {
+            var service = new DogSortService();
+
+            var dogs = new List<Dog>
+            {
+                new Dog { Name = "Neo", Color = "red&amber", TailLength = 22, Weight = 32 },
+                new Dog { Name = "Jessy", Color = "black&white", TailLength = 7, Weight = 14 }
+            }.AsQueryable();
+
+            var sorted = service.ApplySorting(dogs, null, null, new[] { "name", "color", "tail_length", "weight" }).ToList();
+
+            Assert.Equal("Jessy", sorted.First().Name);
+            Assert.Equal("Neo", sorted.Last().Name);
+        }
+
+        [Fact]
+        public void ApplySorting_Should_Sort_Seeded_Dogs_By_Weight_Desc()
+        {
+            var service = new DogSortService();
+
+            var dogs = new List<Dog>
+            {
+                new Dog { Name = "Neo", Color = "red&amber", TailLength = 22, Weight = 32 },
+                new Dog { Name = "Jessy", Color = "black&white", TailLength = 7, Weight = 14 }
+            }.AsQueryable();
+
+            var sorted = service.ApplySorting(dogs, "weight", "desc", new[] { "name", "color", "tail_length", "weight" }).ToList();
+
+            Assert.Equal(32, sorted.First().Weight);
+            Assert.Equal(14, sorted.Last().Weight);
+        }
+
+    }
+}
