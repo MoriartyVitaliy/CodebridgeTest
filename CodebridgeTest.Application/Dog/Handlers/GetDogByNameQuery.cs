047f08f51e754b45b00f8ef2efab2728ab01b440
MoriartyVitaliy <vgriss@knu.ua>
2025-11-05 02:44:38 +0200

    Add query handler for fetching dog by name
    Introduces `GetDogByNameQueryHandler` to handle
    `GetDogByNameQuery` requests, utilizing `IDogRepository`
    to asynchronously retrieve a dog by its name. Also defines
    the `GetDogByNameQuery` record type to encapsulate the
    dog's name and implement the `IRequest` interface for
    returning a nullable `Dog` model.

---
+++ CodebridgeTest.Application/Dog/Handlers/GetDogByNameQuery.cs
@@ -0,0 +1,21 @@
+﻿using CodebridgeTest.Application.Dog.Queries;
+using CodebridgeTest.Core.Interfaces;
+using MediatR;
+
+namespace CodebridgeTest.Application.Dog.Handlers
+{
+    public class GetDogByNameQueryHandler : IRequestHandler<GetDogByNameQuery, Core.Models.Dog?>
+    {
+        private readonly IDogRepository _repository;
+
+        public GetDogByNameQueryHandler(IDogRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<Core.Models.Dog?> Handle(GetDogByNameQuery request, CancellationToken cancellationToken)
+        {
+            return await _repository.GetByNameAsync(request.Name);
+        }
+    }
+}
---
+++ CodebridgeTest.Application/Dog/Queries/GetDogByNameQuery.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+
+namespace CodebridgeTest.Application.Dog.Queries
+{
+    public record GetDogByNameQuery(string Name) : IRequest<Core.Models.Dog?>;
+}
